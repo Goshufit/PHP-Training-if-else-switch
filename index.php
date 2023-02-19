b3d30c75e10130a1ac98f06306c468962491452d
Jean-Pierre Graham <goshufit@gmail.com>
2023-02-18 23:26:09 -0600

    switch statement

--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php 
+$age = 20;
+
+$sentence = $age >= 21 ? "Welcome to the bar here's a bottle" : "Sorry you are not 21 and you can't drink";
+
+switch ($age) {
+    case $age >= 21:
+      echo "Welcome to the bar here's a bottle";
+      break;
+    case $age >= 18 && $age < 21:
+      echo "Welcome to the bar you can chill but can't drink";
+      break;
+    default:
+      echo "Sorry you are not 21 and you can't drink";
+  }
+  
+// if($age >= 21) {
+//     echo "Welcome to the bar here's a bottle";
+// } else if($age >= 18 && $age < 21) {
+// echo "Welcome to the bar you can chill but can't drink";
+// }
+//  else {
+//     echo "Sorry you are not 21 and you can't drink";
+// }
+
 ?>
 
 <!DOCTYPE html>
@@ -11,5 +35,6 @@
 </head>
 <body>
     <h1>Thank God</h1>
+    <h1><?php echo $sentence; ?></h1>
 </body>
 </html>
